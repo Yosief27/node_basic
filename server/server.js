43a4d82c40c8c83a7a8220cdf0c394737a5f5f5e
Yosief27 <josiyoniug@gmail.com>
2021-10-25 20:23:49 +0200

    ock25 eveningwork

--- server/server.js
+++ server/server.js
@@ -4,16 +4,29 @@ const server=http.createServer((req,res)=>{
     //using the req properties like url and method to find out the url content and the way of communiction.i.e get post ...
     console.log(req.url,req.method);
     res.setHeader('Content-Type','text\html');
+    //creating a basic routing for each page you create and statuscode,meaning telling the client the well known error codes like 200 for success or 404 for unsuccessful error.
     let path='./views/';
     switch(req.url){
         case '/':
             path+='index.html';
+            res.statusCode=200;
             break;
         case '/about':
             path+='about.html';
+
+            res.statusCode=200;
+            break;
+
+        case '/aboutyou':
+            res.statusCode=302;
+            res.setHeader('Location','/about');
+            
+            res.end();
             break;
         default :
             path+='404.html';
+            
+            res.statusCode=404
             break;
     }
         fs.readFile(path,(err,data)=>{
