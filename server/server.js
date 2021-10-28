dce6488dc1e147cbc30c5f44ae79a44299589d7c
Yosief27 <josiyoniug@gmail.com>
2021-10-28 15:25:38 +0200

    okt28

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const http=require('http');
 const fs=require('fs');
 const  _=require('lodash');
-const num=_.random(0,20);
-console.log(num)
+
 const server=http.createServer((req,res)=>{
     //using the req properties like url and method to find out the url content and the way of communiction.i.e get post ...
+    console.log(_.random(20,2));
     console.log(req.url,req.method);
     res.setHeader('Content-Type','text\html');
     //creating a basic routing for each page you create and statuscode,meaning telling the client the well known error codes like 200 for success or 404 for unsuccessful error.
@@ -33,6 +33,7 @@ const server=http.createServer((req,res)=>{
             res.statusCode=404
             break;
     }
+    //reading the html file and calling the call back function to use  the data to send to the browser.
         fs.readFile(path,(err,data)=>{
        if (err){
            console.log('file unable to read!');
