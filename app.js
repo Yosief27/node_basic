9f390e71ff664f076411281b801786e9937669b1
Yosief27 <josiyoniug@gmail.com>
2021-11-01 13:11:03 +0100

    from master in skol

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require('express');
 
+
 const app=express();
 //register view engine
 app.set('view engine','ejs');
@@ -9,6 +10,8 @@ app.set('view engine','ejs');
 app.listen(3000,()=>{
     console.log('severing it listening............')
 });
+
+app.use(express.static('source'));
 /*
 app.get(('/main'),(req,res)=>{
     res.send('<p>this is the home page </p>');
@@ -18,8 +21,16 @@ app.get(('/main'),(req,res)=>{
 app.get('/about',(req,res)=>{
     res.send('<p>this is about page!</p>')
 })*/
+//uses to get the basic authontication function and simplar 
+app.use((req,res,next)=>{
+    console.log('host',req.hostname);
+console.log('path',req.url);
+    console.log('method',req.method)
+    next();
+});
+
 app.get(('/'),(req,res)=>{
-    const blogs = [
+        const blogs = [
     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
     {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
     {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
@@ -29,9 +40,15 @@ app.get(('/'),(req,res)=>{
 
 });
 app.get('about/create',(req,res)=>{
+    
+    console.log('path',req.url);
+    console.log('method',req.method)
     res.render('create',{title:'Create a new blog'});
 })
 app.get('/about',(req,res)=>{
+
+    console.log('path',req.url);
+    console.log('method',req.method)
     res.render('about',{title:'about'});
 });
 app.get('/about_us',(req,res)=>{
